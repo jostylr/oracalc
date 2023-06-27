88d5f8238e5bb9778d04c2a1cc4c9ac210237290
James Taylor <jostylr@gmail.com>
2023-06-27 16:54:45 -0400

    first draft of fraction calculator done. Added to_decimal_string which converts fractions to integerEpow10. also added mediant, averge, and some reduce/scale/common function for dealing with multiplicties

--- lib/RatCalc/ratcalc.ex
+++ lib/RatCalc/ratcalc.ex
@@ -1,6 +1,8 @@
 defmodule Ratcalc do
+  @undef {0, 0}
   @moduledoc """
-  Documentation for `Ratcalc`.
+  `Ratcalc` is a module that deals with fractional arithmetic. Its main purpose is to support Intercalc which does interval calculations with fractions.
+  It can do the usual operations as well as compute averages, mediants, and reports a decimal version.
   """
 
   @typedoc """
@@ -20,7 +22,7 @@ defmodule Ratcalc do
     end
   end
 
-  @spec print(tuple, :tex) :: String.t()
+  @spec print(frac, :tex) :: String.t()
   @doc """
   This converts a fraction tuple to a tex fraction string.
   """
@@ -90,5 +92,101 @@ defmodule Ratcalc do
     q
     |> flip()
     |> mul(p)
+  end
+
+  @spec reduce(frac) :: frac
+  @doc """
+  Reduce a fraction if common factors
+  """
+  def reduce({num, den}) do
+    gcd = Integer.gcd(num, den)
+    (gcd === 0 && @undef) || newf(div(num, gcd), div(den, gcd))
+  end
+
+  @spec scale(frac, integer) :: frac
+  @doc """
+  Scales a fraction's numerator and denominator by integer
+  """
+  def scale({num, den}, j) do
+    newf(num * j, den * j)
+  end
+
+  @spec common(frac, frac) :: {frac, frac}
+  @doc """
+  Returns a tuple
+  """
+  def common({pn, pm}, {qn, qm}) do
+    cd = Integer.gcd(pm, qm)
+
+    if cd === 0 do
+      {newf(pn, pm), newf(qn, qm)}
+    else
+      qcd = div(qm, cd)
+      pcd = div(pm, cd)
+      den = qcd * pm
+      {newf(qcd * pn, den), newf(pcd * qn, den)}
+    end
+  end
+
+  @spec average(frac, frac) :: frac
+  @doc """
+  Compute the average of two fractions by adding and dividing by 2
+  """
+  def average(p, q) do
+    add(p, q)
+    |> mul(newf(1, 2))
+  end
+
+  @spec average(list(frac)) :: frac
+  @doc """
+  Averages a list of fractions
+  """
+  def average(fracs) do
+    {sum, n} =
+      Enum.reduce(fracs, {newf(0, 1), 0}, fn frac, {acc, n} -> {add(frac, acc), n + 1} end)
+
+    mul(sum, newf(1, n))
+  end
+
+  @spec mediant(frac, frac) :: frac
+  @doc """
+  Mediant adds the numerators and denominators to get a fraction in between.
+  If using this with 0/0, the mediant returns the original which seems reasonable since it has nowhere else to go. For n/0, it adds n to the numerators, so inching towards the infinity in the denominator increments. Kind of nice.
+  """
+  def mediant({pn, pm}, {qn, qm}) do
+    newf(pn + qn, pm + qm)
+  end
+
+  @spec to_decimal_string(frac, pos_integer(), atom()) :: String.t()
+  @doc """
+  This creates a decimal version of the frac up the number of specified decimal places. This one returns a decimal above the fraction. This is all text as we do not use the decimals for any computation. This makes division by zero easy to handle by being able to give a string representation of infinity
+  """
+
+  def to_decimal_string(frac, places, :gt) do
+    to_decimal_string(frac, places, :ceiling)
+  end
+
+  def to_decimal_string(frac, places, :lt) do
+    to_decimal_string(frac, places, :floor)
+  end
+
+  def to_decimal_string({num, den}, places \\ 5, type) do
+    if den === 0 do
+      cond do
+        num > 0 ->
+          "+Infinity"
+
+        num < 0 ->
+          "-Infinity"
+
+        num === 0 ->
+          "Indeterminate"
+      end
+    else
+      Decimal.Context.with(%Decimal.Context{precision: places, rounding: type}, fn ->
+        Decimal.div(num, den)
+      end)
+      |> Decimal.to_string(:raw)
+    end
   end
 end
--- test/ratcalc_test.exs
+++ test/ratcalc_test.exs
@@ -21,4 +21,46 @@ defmodule RatcalcTest do
     assert(sub(p, q) === {-1, 21})
     assert(divf(p, q) === {14, 15})
   end
+
+  test "reduce" do
+    p = newf(120, 48)
+    q = newf(0, 0)
+    r = newf(3, 0)
+    assert(reduce(p) === {5, 2})
+    assert(reduce(q) === {0, 0})
+    assert(reduce(r) === {1, 0})
+  end
+
+  test "signs" do
+    p = newf(-12, 3)
+    q = newf(12, -3)
+    zz = newf(0, 0)
+    zn = newf(0, -3)
+    nz = newf(-3, 0)
+    assert(p === {-12, 3})
+    assert(q === {-12, 3})
+    assert(zz === {0, 0})
+    assert(zn === {0, 3})
+    assert(nz === newf(-3, 0))
+  end
+
+  test "average" do
+    p = newf(5, 6)
+    q = newf(7, 9)
+    r = newf(-3, 1)
+    assert(average(p, q) === newf(87, 108))
+    assert(average([p, q, r]) === add(p, q) |> add(r) |> mul(newf(1, 3)))
+    assert(average([p, q, r]) === newf(-75, 162))
+  end
+
+  test "mediant" do
+    p = newf(123, 7)
+    q = newf(32, -6)
+    m = mediant(p, q)
+    assert m === mediant(q, p)
+    assert to_decimal_string(m, 8, :gt) === "7"
+    assert mediant(m, q) === {59, 19}
+    assert to_decimal_string(mediant(m, q), 8, :gt) === "31052632E-7"
+    assert to_decimal_string(mediant(m, q), 8, :lt) === "31052631E-7"
+  end
 end
