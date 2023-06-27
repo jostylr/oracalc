087a5a8aaef4cbdb8a17bda4e1861a52a6573053
James Taylor <jostylr@gmail.com>
2023-06-27 10:19:40 -0400

    added dialyzer

--- mix.exs
+++ mix.exs
@@ -6,6 +6,7 @@ defmodule Ratcalc.MixProject do
       app: :ratcalc,
       version: "0.1.0",
       elixir: "~> 1.14",
+      dialyzer: [plt_add_deps: :transitive],
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -21,6 +22,7 @@ defmodule Ratcalc.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:dialyxir, "~> 1.3.0", only: [:dev], runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
