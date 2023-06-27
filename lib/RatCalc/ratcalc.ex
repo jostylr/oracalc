7fd077ff82081f54fd2d194601366930546da6d8
James Taylor <jostylr@gmail.com>
2023-06-27 10:22:00 -0400

    added basic arithmetic of fractions

--- lib/RatCalc/ratcalc.ex
+++ lib/RatCalc/ratcalc.ex
@@ -3,12 +3,21 @@ defmodule Ratcalc do
   Documentation for `Ratcalc`.
   """
 
-  @spec frac(integer(), integer()) :: {integer(), integer()}
-  @doc """
-  Frac creates a 2-tuple for a frac. That's all.
+  @typedoc """
+  Fraction type as numerator, denominator and we do allow 0 denominators
   """
-  def frac(num, den) do
-    {num, den}
+  @type frac :: {integer, integer}
+
+  @spec newf(integer, integer) :: frac
+  @doc """
+  Frac creates a 2-tuple for a frac. It makes sure all denominators are non-negative. Zero is okay!
+  """
+  def newf(num, den) do
+    if den < 0 do
+      {-num, -den}
+    else
+      {num, den}
+    end
   end
 
   @spec print(tuple, :tex) :: String.t()
@@ -26,8 +35,60 @@ defmodule Ratcalc do
   def print(f) do
     "#{elem(f, 0)} / #{elem(f, 1)}"
   end
-end
 
-Ratcalc.frac(22, 9)
-|> Ratcalc.fracString()
-|> IO.puts()
+  @spec add(frac, frac) :: frac
+  @doc """
+  adds two fractions together
+  """
+  def add({pn, pm}, {qn, qm}) do
+    if pm == qm do
+      newf(pn + qn, pm)
+    else
+      newf(pn * qm + qn * pm, pm * qm)
+    end
+  end
+
+  @spec mul(frac, frac) :: frac
+  @doc """
+  multiplies two fractions together
+  """
+  def mul({pn, pm}, {qn, qm}) do
+    newf(pn * qn, pm * qm)
+  end
+
+  @spec neg(frac) :: frac
+  @doc """
+  Negates a fraction
+  """
+  def neg({n, m}) do
+    newf(-n, m)
+  end
+
+  @spec flip(frac) :: frac
+  @doc """
+  Reciprocates a fraction. Again we don't care about zeros so super easy.
+  """
+  def flip({n, m}) do
+    newf(m, n)
+  end
+
+  @spec sub(frac, frac) :: frac
+  @doc """
+  Subtracts two fractions
+  """
+  def sub(p, q) do
+    q
+    |> neg()
+    |> add(p)
+  end
+
+  @spec divf(frac, frac) :: frac
+  @doc """
+  Divides two fractions
+  """
+  def divf(p, q) do
+    q
+    |> flip()
+    |> mul(p)
+  end
+end
--- test/ratcalc_test.exs
+++ test/ratcalc_test.exs
@@ -1,8 +1,24 @@
 defmodule RatcalcTest do
+  import Ratcalc
   use ExUnit.Case
   doctest Ratcalc
 
   test "makes a fraction" do
-    assert Ratcalc.frac(7, 22) == {7, 22}
+    assert newf(7, 22) == {7, 22}
+  end
+
+  test "add two fractions" do
+    assert add(newf(2, 3), newf(4, 5)) === {22, 15}
+  end
+
+  test "easy fraction arithmetic" do
+    p = newf(2, 3)
+    q = newf(5, 7)
+    assert(add(p, q) === {29, 21})
+    assert(mul(p, q) === {10, 21})
+    assert(neg(p) === {-2, 3})
+    assert(flip(p) === {3, 2})
+    assert(sub(p, q) === {-1, 21})
+    assert(divf(p, q) === {14, 15})
   end
 end
