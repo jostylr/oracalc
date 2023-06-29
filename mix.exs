794d6556df40f1bdaa8946719888edde8fa1d147
James Taylor <jostylr@gmail.com>
2023-06-29 17:07:18 -0400

    added exdocs
    the doc generation in elixir is cool

--- mix.exs
+++ mix.exs
@@ -24,7 +24,9 @@ defmodule Ratcalc.MixProject do
     [
       {:dialyxir, "~> 1.3.0", only: [:dev], runtime: false},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
-      {:decimal, "~> 2.1.1"}
+      {:decimal, "~> 2.1.1"},
+      {:benchee, "~> 1.1.0", only: [:dev], runtime: false},
+      {:ex_doc, "~> 0.27", only: :dev, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
